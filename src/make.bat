6e9dbd0af90d7cac7430da5d059d8a6b6c274fce
Cris Silva Jr <ishiikurisu@yahoo.com>
2015-07-15 14:01:22 -0300

    Now reading number of signals

--- src/make.bat
+++ src/make.bat
@@ -1,7 +1,5 @@
-SET SRC=%1
-
 cls
-g++ %SRC% -o emo.exe -lm -O2 -ansi -pipe -Wall
-emo.exe ../data/thiago_emotiv.edf > log.txt
-more log.txt
-del emo.exe log.txt
+g++ emotivedf.cpp -o emo.exe -lm -O2 -ansi -pipe -Wall
+rem emo.exe ../data/thiago_emotiv.edf > log.txt
+emo.exe ../data/1.01.1.1.1.edf > log.txt
+del emo.exe
