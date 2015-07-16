e0c08c75b2810ffdc4c988490067948dc808d9ad
Cris Silva Jr <ishiikurisu@yahoo.com>
2015-07-16 14:49:37 -0300

    Writing columns from *.CSV to *.ASCII

---
+++ notes/thiago.md
@@ -0,0 +1,28 @@
+Processando EDFs do Thiago
+==========================
+
+Tentativa 1
+-----------
+
+converter os *.edf fornecidos para csv.
+
+
+Tentativa 2
+-----------
+
+Usar os arquivos CSV fornecidos por ele e gerar arquivos
+*.ASCII somente dos canais que ele precisa
+
+### Canais necessários
+
+Considerando que o arquivo está 1-indexado, serão
+necessários os seguintes canais (com suas respectivas
+posições relativas):
+
++ FC5: 6
++ FC6: 13
++ T7:  7
++ T8:  12
+
+No arquivo de testes, todos os canais possuem 128
+samples.
---
+++ src/csv_to_ascii.m
@@ -0,0 +1,51 @@
+out = '..\data\ana_data';
+
+% # Reading data
+%
+% assuming they are all the same place...
+% every signal has 128 samples.
+% FC5 is at 6th column (1-index),
+% FC6 is at 13th,
+% T7 is at 7th and
+% T8 is at 12th.
+%
+% to read a *.csv file in matlab, use
+%
+% ``` matlab
+% csvread(inlname, R1, C1, [R1 C1 R2 C2])
+% ```
+%
+% matlab's csvread function is 0-indexed
+% 
+% for FC5,read from 1, 5
+
+inl = strcat(out, '.csv');
+samples = 13400;
+ending = 1 + samples;
+fc5_i = 5;
+fc6_i = 12;
+t7_i = 6;
+t8_i = 11;
+fc5 = csvread(inl, 1, fc5_i, [1, fc5_i, ending, fc5_i]);
+fc6 = csvread(inl, 1, fc6_i, [1, fc6_i, ending, fc6_i]);
+t7  = csvread(inl, 1, t7_i, [1, t7_i, ending, t7_i]);
+t8  = csvread(inl, 1, t8_i, [1, t8_i, ending, t8_i]);
+data = [fc5 fc6 t7 t8];
+
+% # Writing data
+
+fc5_o = strcat(out, '.fc5.ascii');
+fc6_o = strcat(out, '.fc6.ascii');
+t7_o = strcat(out, '.t7.ascii');
+t8_o = strcat(out, '.t8.ascii');
+files = {fc5_o fc6_o t7_o t8_o};
+samples = samples - 1;
+for ii = (1:4)
+	fp = fopen(files{ii}, 'w');
+	for jj = (1:samples)
+		fprintf(fp, '%f, ', data(jj, ii));
+	end
+	fprintf(fp, '%f', data(samples + 1,ii));
+	fclose(fp);
+end
+
