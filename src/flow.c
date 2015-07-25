13a1a98e3be4669f04d261168882c92a30f9a861
Cris Silva Jr <ishiikurisu@yahoo.com>
2015-07-25 16:28:11 -0300

    Created flow.c to help with routine work

---
+++ src/flow.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "joepack.h"
+
+int isnum(char ch)
+{ return (ch >= '0' && ch <= '9')? 1 : 0; }
+
+char* get_new_name(char* old)
+{
+    char* new = "";
+    int limit = strlen(old);
+    int i = -1;
+
+    for (i = 0; isnum(old[i]); ++i)
+        ;
+
+    for (; i < limit; ++i)
+        cat(new, ctos(old[i]));
+
+    return new;
+}
+
+int main(int argc, char const *argv[]) {
+    FILE* files = fopen("files.txt", "r");
+    char* line = read_from_file(files);
+    char* old_name = NULL;
+    char* new_name = NULL;
+    char* command = NULL;
+
+    while (line)
+    {
+        old_name = line;
+        new_name = get_new_name(line);
+
+        printf("%s...", old_name);
+        rename(old_name, new_name);
+        getchar();
+        rename(new_name, old_name);
+
+        line = read_from_file(files);
+    }
+
+    fclose(files);
+    return 0;
+}
