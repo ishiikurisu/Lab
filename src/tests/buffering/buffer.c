926355292e5a22b0e8f9d199b603e6c16683a4d8
Cris Silva Jr <ishiikurisu@yahoo.com>
2015-08-27 11:55:19 -0300

    Testing more buffers... no sucess yet

--- src/tests/buffering/buffer.c
+++ src/tests/buffering/buffer.c
@@ -1,19 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <oa.h>
-#include "buffer.h"
+#include <buffer.h>
 
-int main(int argc, char const *argv[]) {
-    BUFFER *inlet = buffer_new(fopen(argv[1], "r"), 2048);
-    BUFFER *outlet = buffer_new(fopen(argv[2], "w"), 2048);
+int main(int argc, char *argv[]) {
+    BUFFER *inlet = buffer_new(argv[1], "r", 2048);
+    BUFFER *outlet = buffer_new(argv[2], "w", 2048);
     char just_read = ' ';
 
-    while (buffer_is_available(inlet))
+    while (inlet->available)
     {
         just_read = buffer_read(inlet);
         buffer_write(outlet, ctos(just_read));
     }
-    buffer_flush(outlet);
 
     buffer_close(inlet);
     buffer_close(outlet);
