b135164b20e079b8170004430a417c0f1f3911e6
Cris Silva Jr <ishiikurisu@yahoo.com>
2015-10-04 22:03:26 -0300

    Decided colors to use on temporal graph

--- src/SST/temporal/Stopwatch.java
+++ src/SST/temporal/Stopwatch.java
@@ -3,16 +3,24 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Stopwatch
 {
 	private String filePath;
 	private int[] time;
+	private boolean[] answer;
 	private String[] procedure;
+	private HashSet<String> neededFields;
 
 	public Stopwatch(String filePath)
 	{
 		this.filePath = filePath;
+		this.neededFields = this.getNeededFields();
+		this.time = new int[128];
+		this.procedure = new String[128];
+		this.answer = new boolean[128];
+
 		try {
 			this.readData();
 		}
@@ -25,6 +33,35 @@ public class Stopwatch
 	{ return this.time; }
 	public String[] getProcedures()
 	{ return this.procedure; }
+	private HashSet<String> getNeededFields()
+	{
+		HashSet<String> fields;
+
+		fields.add("PressStimulus.RT");
+		fields.add("PressStimulus.ACC");
+		fields.add("SoundStimulus.RT");
+		fields.add("SoundStimulus.ACC");
+		fields.add("VisualStimulus.Duration");
+		fields.add("Procedure[Trial]");
+
+		return fields;
+	}
+
+	private HashMap<String, Integer> getIndex(String line)
+	{
+		HashMap<String, Integer> index = new HashMap<String, Integer>();
+		String[] bits = line.split("\t");
+		String bit;
+
+		for (int i = 0; i < bits.length(); ++i)
+		{
+			if (this.neededFields.contains(bits[i])) {
+				index.add(bits[i], i);
+			}
+		}
+
+		return index;
+	}
 
 	public void readData()
 	throws IOException
@@ -33,40 +70,31 @@ public class Stopwatch
 		InputStream iss = new InputStream(this.filePath);
 		InputStreamReader isr = new InputStreamReader(iss);
 		BufferedReader br = new BufferedReader(isr);
-		String rawData = null;
 		int i = 0;
 
-		this.times = new int[128];
-		this.procedures = new String[128];
-
 		br.readLine(); // read file path
-		rawData = br.readLine(); // get index
-		index = this.getIndex(rawData);
+		index = this.getIndex(br.readLine()); // get index
 		for (i = 0; i < 8; ++i)
 			br.readLine();
 
 		for (i = 0; i < 128; ++i)
-		{
-			/* get information from line */
-		}
+			this.processLine(br.readLine(), i);
 
 		br.close();
 		isr.close();
 		iss.close();
 	}
 
-	private HashMap<String, Integer> getIndexes(String line)
+	private void processLine(String rawData, int numberLine)
 	{
-		HashMap<String, Integer> index = new HashMap<String, Integer>();
-		String[] bits = line.split("\t");
+		String[] data = rawData.split("\t");
+		String procedure = data[this.index.get("Procedure[Trial]")];
 
-		for (int i = 0; i < bits.length(); ++i)
-		{
-			if (bits[i].compareTo("bullshit") == 0) {
-				index.add();
-			}
+		if (procedure.compareTo("PressProc") == 0) {
+			/* add RT information */
 		}
-
-		return index;
+		else if (procedure.compareTo("NotPressProc") == 0) {
+			/* add SSD information */
+		}
 	}
 }
--- src/SST/temporal/temporal.pde
+++ src/SST/temporal/temporal.pde
@@ -1,13 +1,13 @@
 final int HEIGHT = 1000;
 final int WIDTH = 128*4;
-final color BLUE;
-final color RED;
-final color GREEN;
+final color BLUE = color(0, 90, 146); // PANTONE's Imperial blue, 19-4245 TPX
+final color RED = color(208, 28, 31); // PANTONE's Fiery red, 18-1664 TPX
+final color GREEN = color(126, 200, 69); // PANTONE's Jasmine Green
 int[] time;
 String[] procedure;
 
 void setup() {
-	size(WIDTH, HEIGHT, P2D);
+	size(WIDTH, HEIGHT);
 	noLoop();
 }
 
@@ -22,6 +22,9 @@ void draw() {
 
 	for (int i = 0; i < 128; ++i)
 	{
+		/* decide color */
+
+		/* write lines */
 		if (procedure[i].compareTo("PressProc") == 0) {
 			/* show RT */
 		}
