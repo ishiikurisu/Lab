ac1893be6ab3fd467410d9925a709093adee6269
Cris Silva Jr <ishiikurisu@yahoo.com>
2015-10-29 10:54:36 -0200

    Setting up goals for public exam

---
+++ src/tests/goal/Goal.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class Goal
+{
+	static String data;
+
+	public static void main(String[] args) 
+	throws IOException
+	{
+		System.setProperty("file.encoding", "Cp1252");
+		setup();
+		draw();
+	}
+
+	static void setup()
+	throws IOException
+	{
+		BufferedReader inlet = new BufferedReader(
+			new InputStreamReader(System.in)
+		);
+		String line;
+
+		for (line = inlet.readLine(); 
+			 line != null; 
+			 data += line + " ", line = inlet.readLine());
+
+		inlet.close();
+	}
+
+	static void draw()
+	throws IOException
+	{
+		for (String part: data.split(";"))
+			System.out.println("- [ ] " + part);
+	}
+}
