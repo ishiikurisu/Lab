592c89a1dcd238a975bbbbbb487e32be97ade9c5
Cris Silva Jr <ishiikurisu@yahoo.com>
2015-12-15 11:48:01 -0200

    Let's make Clojure work with Java

---
+++ src/tests/clj2java/Big.java
@@ -0,0 +1,9 @@
+import tiny;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("(binomial 5 3): " + tiny.binomial(5, 3));
+        System.out.println("(binomial 10042, 111): " + tiny.binomial(10042, 111));
+    }
+}
