89c51803c7f25cc95ac16781b4d992292c241a30
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-01-11 17:47:28 -0200

    Updating .gitignore

---
+++ src/tests/clj2java/rocks.crisjr.tiny/target/Big.java
@@ -0,0 +1,9 @@
+import rocks.crisjr.tiny;
+
+public class Big {
+
+    public static void main(String[] args) {
+        System.out.println("(binomial 5 3): " + tiny.binomial(5, 3));
+        System.out.println("(binomial 10042, 111): " + tiny.binomial(10042, 111));
+    }
+}
