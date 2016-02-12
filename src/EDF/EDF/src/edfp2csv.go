fc1d27c1304738a15ac06d41922b29e9c7b2b284
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-02-12 11:44:13 -0200

    Ready to ready the data records

--- src/EDF/EDF/src/edfp2csv.go
+++ src/EDF/EDF/src/edfp2csv.go
@@ -6,7 +6,8 @@ import "fmt"
 
 func main() {
     input := os.Args[1]
+    header, records := edfp.ReadFile(input)
 
     fmt.Printf("input: %v\n", input)
-    fmt.Printf("specs: %#v\n", edfp.ReadFile(input))
+    fmt.Printf("specs: %#v %#v\n", header, records)
 }
---
+++ src/EDF/EDF/src/script.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	joe := make([][]int, 3)
+
+	for i := 0; i < 3; i++ {
+		joe[i] := make([]int, 5+i)
+	}
+
+	fmt.Printf("%#v", joe)
+}
