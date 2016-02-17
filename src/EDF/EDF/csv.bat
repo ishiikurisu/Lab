6bb54fa8c0d831ed285eed9632b00794ecb5f3b5
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-02-17 16:23:10 -0200

    Almost finished this program

--- src/EDF/EDF/csv.bat
+++ src/EDF/EDF/csv.bat
@@ -2,5 +2,5 @@ cd src
 mingw32-make all
 move *.exe ..
 cd..
-edfp2csv data\teste.edf
-del *.exe
+edfp2csv data\teste.edf > data\teste.csv
+REM del *.exe
--- src/EDF/EDF/src/edfp/edfpWrite.go
+++ src/EDF/EDF/src/edfp/edfpWrite.go
@@ -9,23 +9,44 @@ func str2int64(str string) int64 {
 	return x
 }
 
-func convert(header map[string]string, which int, value int16) float32 {
-	dmax := str2int64(header["digitalmaximum"])
-	dmin := str2int64(header["digitalminimum"])
-	pmax := str2int64(header["physicalmaximum"])
-	pmin := str2int64(header["physicalminimum"])
-	return 0.0
+func setConvertionFactor(header map[string]string) []float32 {
+	ns := getNumberSignals(header)
+	factors := make([]float32, ns)
+	dmaxs := separateString(header["digitalmaximum"], ns)
+	dmins := separateString(header["digitalminimum"], ns)
+	pmaxs := separateString(header["physicalmaximum"], ns)
+	pmins := separateString(header["physicalminimum"], ns)
+
+	for i := 0; i < ns; i++ {
+		dmax := str2int64(dmaxs[i])
+		dmin := str2int64(dmins[i])
+		pmax := str2int64(pmaxs[i])
+		pmin := str2int64(pmins[i])
+		factors[i] = float32(dmax-dmin)/float32(pmax-pmin);
+	}
+
+	return factors
 }
 
 /* --- AUXILIAR FUNCTIONS --- */
+func separateString(stuff string, howMany int) []string {
+	bit := len(stuff) / howMany
+    outlet := make([]string, howMany)
+
+    for i := 0; i < howMany; i++ {
+    	outlet[i] = stuff[bit*i:bit*i+bit]
+    }
+
+    return outlet
+}
+
 func getLabels(header map[string]string) string {
 	numberSignals := getNumberSignals(header)
-    labels := header["label"]
-    ls := len(labels) / numberSignals // labelSize
-    outlet := ""
+	labels := separateString(header["label"], numberSignals)
+    outlet := labels[0]
 
-    for i := 0; i < numberSignals; i++ {
-    	outlet += labels[ls*i:ls*i+ls] + " "
+    for i := 1; i < numberSignals; i++ {
+    	outlet += " " + labels[i]
     }
 
     return outlet
@@ -48,6 +69,7 @@ func WriteGo(header map[string]string, records [][]int16) {
  */
 func WriteCSV(header map[string]string, records [][]int16) {
 	numberSignals := getNumberSignals(header)
+	convertionFactor := setConvertionFactor(header)
 
 	// writing header...
 	fmt.Printf("title:%s,", header["recording"])
@@ -59,18 +81,17 @@ func WriteCSV(header map[string]string, records [][]int16) {
 	fmt.Printf("units:uV\n")
 
 	// writing data records...
-	// TODO: create an array of correction factors
 	limit := len(records[0])
 	for j := 0; j < limit; j++ {
 		for i := 0; i < numberSignals; i++ {
-			data := convert(header, i, records[i][j])
+			data := float32(records[i][j]) * convertionFactor[i]
 
 			if i == 0 {
 				fmt.Printf("%f", data)
 			} else {
-				fmt.Printf(" %f", data)
+				fmt.Printf(", %f", data)
 			}
-		}		
+		}
 		fmt.Printf("\n")
 	}
 }
