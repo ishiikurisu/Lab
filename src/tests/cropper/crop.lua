6bb5a37720ce6ac65d38b38083473caebcdaaf60
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-03-01 10:25:07 -0300

    Working on Cropper

--- src/tests/cropper/crop.lua
+++ src/tests/cropper/crop.lua
@@ -1,16 +1,20 @@
-imagemagick = function(img)
-	local resize = string.format("convert %s.png -resize 90%% %s.jpg", img, img)
-	local crop0 = string.format("convert %s.jpg -crop 339x233+73+17 %s.0.png", img, img)
-	local crop1 = string.format("convert %s.jpg -crop 134x100+15+278 %s.1.png", img, img)
-	-- local crop2
-	-- local crop3
-	-- local crop4
-	-- local crop5
-	-- local crop6
-	local clean = "del *.jpg"
+local cropper = { }
 
-	os.execute(resize)
-	os.execute(crop0)
-	os.execute(crop1)
-	os.execute(clean)
+-- requires imagemagick installed
+function cropper.magic(img)
+	-- AxB+C+D creates a A pixels x B pixels image starting at (C, D)
+	op = { }
+	op[1] = string.format("convert %s.jpg -crop 1397x1064+870+345 %s.0.png", img, img)
+	op[2] = string.format("convert %s.jpg -crop 540x350+513+1470 %s.1.png", img, img)
+	op[3] = string.format("convert %s.jpg -crop 540x350+1308+1470 %s.2.png", img, img)
+	op[4] = string.format("convert %s.jpg -crop 540x350+2153+1470 %s.3.png", img, img)
+	op[5] = string.format("convert %s.jpg -crop 540x350+513+1900 %s.4.png", img, img)
+	op[6] = string.format("convert %s.jpg -crop 540x350+1308+1900 %s.5.png", img, img)
+	op[7] = string.format("convert %s.jpg -crop 540x350+2153+1900 %s.6.png", img, img)
+
+	for _, v in ipairs(op) do
+		os.execute(v)
+	end
 end
+
+return cropper
---
+++ src/tests/cropper/main.lua
@@ -0,0 +1,4 @@
+local cropper = require "crop"
+cropper.magic("raven002")
+cropper.magic("raven003")
+cropper.magic("raven004")
