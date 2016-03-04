9c9c63171fdf4de5cce0578151a847361676dea9
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-03-04 09:58:39 -0300

    I guess it was indeed the lack of a high pass filter

--- src/EDF/EDF/all.bat
+++ src/EDF/EDF/all.bat
@@ -1,11 +1,10 @@
 cls
 REM build edf2csv
 cd src
-mingw32-make edf2csv
+mingw32-make all
 move *.exe ..
 cd..
 
 REM translate data
 rem edf2ascii data\joe-teste-17.02.16.13.56.33.edf > "C:\Documents and Settings\Laboratorio.LABORATO-6A7377\Meus documentos\software\eeglab\joe.ascii"
-edf2csv data\joe-teste-17.02.16.13.56.33.edf > data\joe.csv
-csv2ascii -m data\joe.csv
+edf2ascii data\joe-teste-17.02.16.13.56.33.edf > data\joe.ascii
