ea0f8d5444e6312682f82b0d4418f114155e3c02
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-03-07 08:24:34 -0300

    Writing MATLAB interface for edf2ascii binary

--- src/EDF/protolize/src/interface/script.m
+++
@@ -1,5 +0,0 @@
-% Let's try to write an interface to P2
-% TODO: write a function that returns the available channels from
-%       an EDF file into a string cell, and let the user choose 
-%       which one to load. An edfutil folder must contain everything
-%       this script needs to run by itself. 
