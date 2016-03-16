168902428e118a9bce74344a36fadb8b09b6d877
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-03-16 11:50:22 -0300

    I think I'm committing a bug here

--- src/SST/analyze/go/sst/SSRTCalculator.go
+++ src/SST/analyze/go/sst/SSRTCalculator.go
@@ -1,2 +1,12 @@
 package sst
 
+func GetNeedVariables() []string {
+	return {
+		"PressStimulus.RT",
+		"PressStimulus.ACC",
+		"SoundStimulus.RT",
+		"SoundStimulus.ACC",
+		"VisualStimulus.Duration",
+		"Procedure[Trial]"	
+	}
+}
--- src/SST/analyze/go/sst/SSTReader.go
+++ src/SST/analyze/go/sst/SSTReader.go
@@ -15,14 +15,23 @@ func Read(input string) map[string]int {
 	return header
 }
 
+/**
+ * Gets the indexes of the needed fields from the header
+ * @param inlet the pointer to the specified file
+ * @return a map relating the indexes and the fields
+ */
 func ReadHeader(inlet *os.File) map[string]int {
 	ReadLine(inlet)
 	rawData := ReadLine(inlet)
 	header := make(map[string]int)
 	stuff := Split(rawData, '\t')
+	needed := GetNeededVariables()
 
 	header["raw"] = len(rawData)
 	header["stuff"] = len(stuff)
+	for i, it := range needed {
+		fmt.Printf("%d. %v\n", i, it)
+	}
 
 	return header
 }
