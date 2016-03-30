d6a08072ff89f1fd2a969cb4af0372844876b2cb
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-03-30 10:32:29 -0300

    This algorithm is taking too long

--- src/P2EXT/edfp2ascii/src/edfp2ascii/convert_edfp2csv.m
+++ src/P2EXT/edfp2ascii/src/edfp2ascii/convert_edfp2csv.m
@@ -2,4 +2,59 @@
 %% CSV channels file and a TXT annotations files
 function [status, cmdout] = convert_edfp2csv(inputfile)
 fprintf('edf+ to csv\n');
-[status, cmdout] = system(sprintf('"%s\\edfp2ascii\\edfp2csv" "%s"', cd, inputfile))
+[status, cmdout] = system(sprintf('"%s\\edfp2ascii\\edfp2csv" "%s"', cd, inputfile));
+% TODO separate the annotations from the csv file in the cmdout variable
+%      and save them into two different files.
+[csv notes] = separate_csv_from_notes(cmdout);
+notes
+
+function [chops] = split_string(inlet, separator)
+limit = length(inlet);
+chops = {};
+chop = '';
+index = 1;
+
+while index <= limit
+	it = inlet(index);
+	if isequal(it, separator)
+		chops{length(chops)+1} = chop;	
+		chop = '';
+	else
+		chop = [chop it];
+	end
+
+	index = index+1;
+end
+
+if length(chop) > 0
+	chops{length(chops+1)} = chop;
+end
+
+%% starts_with: checks if the first string begins with the other string
+function [result] = starts_with(haystack, needle)
+result = true;
+
+for index = 1:length(needle)
+	if ~isequal(haystack(index), needle(index))
+		result = false;
+		break;
+	end
+end
+
+%% separate_csv_from_notes: it does what it promises.
+function [csv, notes] = separate_csv_from_notes(inlet)
+fprintf('separating stuff...\n');
+lines = split_string(inlet, '\n');
+index = 1;
+
+csv = '';
+while ~starts_with(lines{index}, '---')
+	csv = [csv lines{index} '\n'];
+	index = index+1;
+end
+
+notes = '';
+while index < length(lines)
+	notes = [notes lines{index} '\n'];
+	index = index+1;
+end
--- src/P2EXT/edfp2ascii/src/edfp2ascii/edfp2csv.cpp
+++ src/P2EXT/edfp2ascii/src/edfp2ascii/edfp2csv.cpp
@@ -1,40 +1,13 @@
 #include "edfp/edfp.hpp"
 
-const char* generate_output(const char *input, const char *extension) 
-{
-	std::string inlet = input;
-	std::string outlet;
-	int offset = inlet.length() - 1;
-
-	while (inlet.at(offset) != '.')
-		offset--;
-
-	for (int i = 0; i < inlet.length() - offset; ++i)
-		outlet += inlet.at(i);
-	outlet += extension;
-
-	return outlet.c_str();
-}
-
-void write_annotations(EDFP edfp, const char *input)
-{
-	const char *output = generate_output(input, ".txt");
-	FILE *outlet = fopen(output, "w");
-
-	if (outlet == NULL) outlet = stdout;
-	fprintf(outlet, "--- # Annotations\n%s\n...\n", edfp.get_annotations());
-
-	fclose(outlet);
-}
-
 int main(int argc, char const *argv[])
 {
-	const char *input = argv[1];
 	EDFP edfp;
 
-	edfp.read_file(input);
-	edfp.csv(generate_output(input, ".csv"));
-	write_annotations(edfp, input);
+	edfp.read_file(argv[1]);
+	edfp.csv();
+	printf("--- # Annotations\n%s\n...\n", 
+		   edfp.get_annotations());
 
 	return 0;
 }
