7cffb6e1a1acda66abc1c8e81df479ae5390d1d6
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-04-05 14:30:08 -0300

    Removing debug messages

--- src/P2EXT/edfp2ascii/src/edfp2ascii/convert_edfp2csv.m
+++ src/P2EXT/edfp2ascii/src/edfp2ascii/convert_edfp2csv.m
@@ -1,7 +1,6 @@
 %% convert_edfp2csv: Converts an EDF+ file to a 
 %% CSV channels file and a TXT annotations files
 function [status, cmdout] = convert_edfp2csv(inputfile)
-fprintf('edf+ to notes\n');
 [status, cmdout] = system(sprintf('"%s\\edfp2ascii\\edfp2notes" "%s"', ...
 	                              cd, ...
 	                              inputfile));
@@ -9,7 +8,6 @@ fp = fopen(generate_name(inputfile, '.txt'), 'w');
 fprintf(fp, '%s\n', cmdout);
 fclose(fp);
 
-fprintf('edf+ to csv\n');
 [status, cmdout] = system(sprintf('"%s\\edfp2ascii\\edfp2csv" "%s"', cd, inputfile));
 fp = fopen(generate_name(inputfile, '.csv'), 'w');
 fprintf(fp, '%s\n', cmdout);
--- src/P2EXT/edfp2ascii/src/edfp2ascii/edfp2notes.cpp
+++ src/P2EXT/edfp2ascii/src/edfp2ascii/edfp2notes.cpp
@@ -5,7 +5,7 @@ int main(int argc, char const *argv[])
 	EDFP edfp;
 
 	edfp.read_file(argv[1]);
-	printf("\n--- # Annotations\n%s\n...\n", 
+	printf("--- # Annotations\r\n%s\r\n...\r\n", 
 		   edfp.get_annotations());
 
 	return 0;
