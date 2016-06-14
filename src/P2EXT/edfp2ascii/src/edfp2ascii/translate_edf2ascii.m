218be63a3adcbcde64ba73e3f1b9a7fb59a5703a
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-06-14 17:36:01 -0300

    Moving MATLAB scripts to github.com/ishiikurisu/P2

--- src/P2EXT/edfp2ascii/src/edfp2ascii/translate_edf2ascii.m
+++
@@ -1,6 +0,0 @@
-%% translate_edf2ascii: Decides if it should use single or multiple translation
-function [errorcode] = translate_edf2ascii(inputfile, is_multiple)
-% inputfile    a string containing the input file
-% is_multiple  how this file should be translated
-convert_edfp2csv(inputfile);
-errorcode = convert_csv2ascii(inputfile, is_multiple);
