5b827d5120cd77b2ba55c6d22ca87288cc681d8b
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-07-05 13:34:17 -0300

    Made a simple board. Really stupid, actually

--- src/Drawing/Board/Board/MainWindow.xaml.cs
+++ src/Drawing/Board/Board/MainWindow.xaml.cs
@@ -20,39 +20,87 @@ namespace Board
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Brush BrushColor { get; set; }
+        private double BrushSize { get; set; }
+        private List<Ellipse> Painting { get; set; }
+        private bool IsDrawing { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            BrushColor = Brushes.Black;
+            Painting = new List<Ellipse>();
+            BrushSize = 5;
+            IsDrawing = false;
         }
 
-        private void canvasBoard_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private async void canvasBoard_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            var canvas = sender as Canvas;
+            var where = e.GetPosition(canvas);
+            var paint = CreatePaint(where);
 
+            Painting.Add(paint);
+            canvas.Children.Add(paint);
+            IsDrawing = true;
+            await StopDrawing();
+        }
+
+        private void canvasBoard_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            IsDrawing = false;
+        }
+
+        private async Task<bool> StopDrawing()
+        {
+            var canvas = this.canvasBoard;
+            var where = Mouse.GetPosition(canvas);
+
+            while (IsDrawing)
+            {
+                where = Mouse.GetPosition(canvas);
+                var paint = CreatePaint(where);
+                canvas.Children.Add(paint);
+                await Task.Delay(5);
+            }
+
+            return IsDrawing;
         }
 
         private void buttonBlack_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-
+            BrushColor = Brushes.Black;
         }
 
         private void buttonRed_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-
+            BrushColor = Brushes.Red;
         }
 
-        private void button_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void buttonBlue_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-
+            BrushColor = Brushes.Blue;
         }
 
         private void buttonGreen_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-
+            BrushColor = Brushes.Green;
         }
 
         private void buttonWhite_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            BrushColor = Brushes.White;
+        }
 
+        private Ellipse CreatePaint(Point where)
+        {
+            var paint = new Ellipse();
+
+            paint.Fill = BrushColor;
+            paint.Margin = new Thickness(where.X, where.Y, 0, 0);
+            paint.Width = paint.Height = BrushSize;
+
+            return paint;
         }
     }
 }
