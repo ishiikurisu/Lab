32fbdc79bdea06064169343be6577c7013481834
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-07-08 12:26:36 -0300

    Tried to compile PACI on Molang without success

--- src/tests/MoreClasses/App.cs
+++ src/tests/MoreClasses/App.cs
@@ -13,15 +13,16 @@ namespace MoreClasses
 
 			while (line.Length > 1)
 			{
-				var stuff = new List<string>
-				                    (line.Split(new Char[] {':'})
-				                         .Select((item) => Clean(item)));
+				var stuff = new List<string>(line.Split(new Char[] {':'})
+				                                 .Select((item) => Clean(item)));
 				Console.WriteLine(string.Format("  {0}\n  {1}", 
 					                            stuff.ElementAt(0),
 					                            stuff.ElementAt(1)));
 				Boxes.Add(new Box(stuff.ElementAt(0), stuff.ElementAt(1)));
 				line = Console.ReadLine();
 			}
+
+			// TODO: Add class (name it Bird) to retrieve only tags from boxes
 		}
 
 		private static string Clean(string inlet)
