a4bb149e669a699a582ef92af53b16705859637f
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-08-24 15:13:02 -0300

    Calling Java from MATLAB

---
+++ src/tests/matjava/coffee.m
@@ -0,0 +1,8 @@
+function coffee()
+disp('Let''s call java from matlab');
+disp('Adding path...');
+addpath(pwd);
+disp('Testing...')
+salute = donuts.Donut.sayHi('Joe');
+disp(salute);
+disp('Ok!');
---
+++ src/tests/matjava/donuts/Donut.java
@@ -0,0 +1,14 @@
+package donuts;
+
+public class Donut
+{
+    public static String sayHi(String name)
+    {
+        return "Hello " + name + " from Donut class!";
+    }
+
+    public static final void main(String[] args)
+    {
+        System.out.println("Hello Donut!");
+    }
+}
