f4363563c52f2728cb035054100b711b5bb4a5e2
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-09-27 17:30:25 -0300

    Trying to use br.unb.biologiaanimal.edf package on MATLAB

--- src/tests/jedf/yoda.m
+++ src/tests/jedf/yoda.m
@@ -7,15 +7,16 @@ javaaddpath(strcat(pwd, '/edf.jar'));
 if exist(inlet, 'file') == 2
 	fprintf('# Loading files\n');
 	edf = br.unb.biologiaanimal.edf.EDF(inlet);
+	outlet = edf;
 	fprintf('# Displaying data\n');
 	labels = edf.getLabels();
 	fprintf('Labels:\n');
 	for n = 1:length(labels)
-		fprintf('- %s\n', label(n));
+		fprintf('- %s\n', labels(n)); % TODO Discover why this line doesn't work
 	end
 	fprintf('Convertion factors:\n')
 	for n = 1:length(labels)
-		fprintf('- %s: %s\n', label(n), edf.getConvertionFactor(label));
+		fprintf('- %s: %s\n', labels(n), edf.getConvertionFactor(labels(n)));
 	end
 	plot(edf.getSignal(labels(1)));
 else
