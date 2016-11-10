c797d4961db48434c8e57e46dd9e3009e313903b
Cristiano Silva Jr <cristianoalvesjr@gmail.com>
2016-11-10 16:06:59 -0200

    Building a table from a TSV file

--- src/ids/creator.lua
+++ src/ids/creator.lua
@@ -1,5 +1,6 @@
 local creator = { }
 
+--[[ Latex functions ]]
 creator.createHeader = function()
   return [[
 \documentclass{article}
@@ -14,6 +15,7 @@ creator.createTail = function()
 ]]
 end
 
+-- [[ IO functions ]]
 creator.loadData = function(source)
   local lines = { }
 
@@ -24,10 +26,65 @@ creator.loadData = function(source)
   return table.concat(lines, '\n')
 end
 
+creator.buildTable = function(raw)
+  local outlet = { }
+  local data = { }
+  local labels = { }
+  local lines = strsplit(raw, '\n')
+  local limit = -1
+
+  -- Extracting fields from raw data
+  for _, line in pairs(lines) do
+    local columns = strsplit(line, '\t')
+    table.insert(data, columns)
+  end
+
+  -- Building map
+  labels = data[1]
+  limit = #data - 1
+
+  for j = 2, limit do
+    for i, label in pairs(labels)  do
+      if outlet[label] == nil then
+        outlet[label] = { }
+      end
+      table.insert(outlet[label], data[j][i])
+    end
+  end
+
+  return outlet
+end
+
+creator.table2string = function(matrix)
+  local outlet = { }
+
+  for tag, line in pairs(matrix) do
+    table.insert(outlet, tag .. ": " .. table.concat(line, "; "))
+  end
+
+  return table.concat(outlet, '\n')
+end
+
+-- [[ Main functions ]]
+function strsplit(inputstr, sep)
+  -- http://stackoverflow.com/questions/1426954/split-string-in-lua
+  local t = {}; i = 1
+  if sep == nil then sep = "%s" end
+  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+    t[i] = str
+    i = i + 1
+  end
+  return t
+end
+
 creator.create = function(source)
   local outlet = creator.createHeader()
-  outlet = outlet .. creator.loadData(source) .. '\n'
+  local raw = creator.loadData(source)
+  local data = creator.buildTable(raw)
+
+  outlet = outlet .. creator.table2string(data) .. '\n'
   outlet = outlet .. creator.createTail()
+
   return outlet
 end
 
