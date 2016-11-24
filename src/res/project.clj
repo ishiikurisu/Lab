164de249b2def0451e4d03f46cf29833c161b6ec
Cristiano Silva Jr <cristianoalvesjr@gmail.com>
2016-11-24 13:25:42 -0200

    Final version is working

--- src/res/project.clj
+++ src/res/project.clj
@@ -1,9 +1,9 @@
 (defproject res "0.1.0"
   :description "Creates a table from a YAML file"
-  :url "http://example.com/FIXME"
+  :url "http://www.crisjr.eng.br"
   :license {:name "THE BEER-WARE LICENSE"
             :url "https://people.freebsd.org/~phk/"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot res.core
+  :main res.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
