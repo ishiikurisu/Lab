cc33325b08811659d0dd9638e595de9c52af76bc
Cristiano Silva Jr <cristianoalvesjr@gmail.com>
2016-12-14 10:29:29 -0200

    Cleaning some dead code

--- src/tests/uianswer/main.m
+++ src/tests/uianswer/main.m
@@ -1,4 +1,4 @@
 function main
 % Testing if I can get a value from a GUIDE window
-message = message_ui('get');
+message = message_ui;
 disp(message);
--- src/tests/uianswer/message_ui.m
+++ src/tests/uianswer/message_ui.m
@@ -1,29 +1,4 @@
 function varargout = message_ui(varargin)
-% MESSAGE_UI M-file for message_ui.fig
-%      MESSAGE_UI, by itself, creates a new MESSAGE_UI or raises the existing
-%      singleton*.
-%
-%      H = MESSAGE_UI returns the handle to a new MESSAGE_UI or the handle to
-%      the existing singleton*.
-%
-%      MESSAGE_UI('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in MESSAGE_UI.M with the given input arguments.
-%
-%      MESSAGE_UI('Property','Value',...) creates a new MESSAGE_UI or raises the
-%      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before message_ui_OpeningFcn gets called.  An
-%      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to message_ui_OpeningFcn via varargin.
-%
-%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
-%      instance to run (singleton)".
-%
-% See also: GUIDE, GUIDATA, GUIHANDLES
-
-% Edit the above text to modify the response to help message_ui
-
-% Last Modified by GUIDE v2.5 14-Dec-2016 09:36:54
-
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
@@ -46,28 +21,11 @@ end
 
 % --- Executes just before message_ui is made visible.
 function message_ui_OpeningFcn(hObject, eventdata, handles, varargin)
-% This function has no output args, see OutputFcn.
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to message_ui (see VARARGIN)
-
-% Choose default command line output for message_ui
-handles.running = true;
-
-% Update handles structure
 guidata(hObject, handles);
-
-% UIWAIT makes message_ui wait for user response (see UIRESUME)
 uiwait(handles.figure1);
-
 
 % --- Outputs from this function are returned to the command line.
 function varargout = message_ui_OutputFcn(hObject, eventdata, handles)
-% varargout  cell array for returning output args (see VARARGOUT);
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
 varargout{1} = handles.output;
 close(handles.figure1);
 
@@ -82,12 +40,6 @@ function editMessage_Callback(hObject, eventdata, handles)
 
 % --- Executes during object creation, after setting all properties.
 function editMessage_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to editMessage (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
 if ispc && isequal(get(hObject,'BackgroundColor'), ...
                    get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
@@ -96,10 +48,6 @@ end
 
 % --- Executes on button press in pushbuttonOk.
 function pushbuttonOk_Callback(hObject, eventdata, handles)
-% hObject    handle to pushbuttonOk (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
 handles.output = get(handles.editMessage, 'String');
-handles.running = false;
 guidata(hObject, handles);
 uiresume(handles.figure1);
