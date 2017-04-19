55dd693b792495bdde60655becc6fb97f45e9ecf
Cristiano Silva Jr <cristianoalvesjr@gmail.com>
2017-04-19 08:13:51 -0300

    Reading signals from ASCII file

--- src/spectropy/main.ascii.py
+++ src/spectropy/main.ascii.py
@@ -2,6 +2,23 @@ __ABOUT__ = """
 This script is a line-filter for generating spectrograms from ASCII files. It
 is expected these files have already been filtered from MATLAB.
 """
+
+def read():
+    outlet = { }
+
+    while True:
+        try:
+            line = map(float, input().rstrip().lstrip().split('\t'))
+            for i, it in enumerate(line):
+                if i not in outlet:
+                    outlet[i] = [ ]
+                outlet[i].append(it)
+        except EOFError:
+            break
+
+    return outlet
+
 if __name__ == '__main__':
     # TODO Load files to a dict
+    data = read()
     # TODO Plot files as we do with main.csv.py
