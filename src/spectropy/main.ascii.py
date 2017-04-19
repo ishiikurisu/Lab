6984dbdb520785be1954e83b70b5b8ed5266e4e7
Cristiano Silva Jr <cristianoalvesjr@gmail.com>
2017-04-19 08:08:21 -0300

    Preparing spectropy for data analysis

---
+++ src/spectropy/main.ascii.py
@@ -0,0 +1,7 @@
+__ABOUT__ = """
+This script is a line-filter for generating spectrograms from ASCII files. It
+is expected these files have already been filtered from MATLAB.
+"""
+if __name__ == '__main__':
+    # TODO Load files to a dict
+    # TODO Plot files as we do with main.csv.py
