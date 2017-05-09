ce2bd14ca13e8f88fceef4b6b8b76a815c8557a7
Cristiano Silva Jr <cristianoalvesjr@gmail.com>
2017-05-09 12:31:09 -0300

    Trying to read an EDF file using Python

---
+++ src/spectropy/readedf.py
@@ -0,0 +1,8 @@
+import pyedflib
+import numpy as np
+f = pyedflib.EdfReader("camila.edf")
+n = f.signals_in_file
+signal_labels = f.getSignalLabels()
+sigbufs = np.zeros((n, f.getNSamples()[0]))
+for i in np.arange(n):
+    sigbufs[i, :] = f.readSignal(i)
