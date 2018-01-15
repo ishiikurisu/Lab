4deb22ebb1b74d6ba4705bfaa4f94210a7e64de9
crs94 <rscamila1994@gmail.com>
2018-01-15 10:54:39 -0200

    I don't know how to deal with this HTML form for files

--- src/worker/main/main.go
+++ src/worker/main/main.go
@@ -7,10 +7,16 @@ import (
 
 func main() {
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/e2c", e2c)
 
 	http.ListenAndServe(":3000", nil)
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Say hello to server!")
+}
+
+func e2c(w http.ResponseWriter, r *http.Request) {
+	// TODO Think about how to load a HTML form with a file input
+	// TODO Deal with the file input and write the output in the form of a separate file
 }
